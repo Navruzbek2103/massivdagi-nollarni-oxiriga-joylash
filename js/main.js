1b3a1f31f1f47893b953e000cefa5a444d722341
Navruzbek2103 <navrozbek2306@gmail.com>
2022-08-24 17:47:46 +0500

    Finished app

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
-let numbers = [1, 3, 0, 5, 0, 7, 0, 2, 0, 6, 0, 0, 87, 4];
+let numbers = [1, 3, 0, 5, 0, 7, 0, 2, 0, 6, 4];
 
+let elBody = document.querySelector("body");
 let elContainer = document.createElement("div");
 let elTitle = document.createElement("h2");
 let elSubtitle = document.createElement("h3");
-let elResult = document.createElement("")
+let elResult = document.createElement("p");
+
+
+
+elContainer.appendChild(elTitle)
+elContainer.appendChild(elSubtitle)
+elContainer.appendChild(elResult)
+elBody.appendChild(elContainer)
+
+elContainer.style.backgroundColor = "bisque";
+elContainer.style.maxWidth = "350px";
+elContainer.style.width = "100%";
+elContainer.style.borderRadius = "10px";
+elContainer.style.padding = "10px";
+elContainer.style.margin = "10px auto";
+elContainer.style.display = "flex";
+elContainer.style.flexDirection = "column";
+elContainer.style.alignItems = "center";
+elContainer.style.textAlign = "center";
+
+elTitle.textContent = "Bu dastur massivning elementlari orasidagi 0 ni eng oxiriga qo'yib beradi";
+elTitle.style.fontSize = "25px";
+elTitle.style.margin = "0";
+
+
+elSubtitle.textContent = "Massiv elementlari: " + numbers
 
 function zeroEnd(num){
   let count = 0;
@@ -21,9 +47,9 @@ function zeroEnd(num){
   for(let j = 0; j < count; j++){
     array.push(0)
   }
-  console.log(array.join(""));
+  elResult.textContent = "Natija: " + array.join("")
 }
 
 
 
-zeroEnd(numbers);
+zeroEnd(numbers)
